7c4d0f4054a0526b4c2971d01b0d37a19cb38433
Rafael <rafaelrehfeld97@gmail.com>
2023-06-26 20:19:05 -0300

    ok

--- src/components/Repositorio.jsx
+++ src/components/Repositorio.jsx
@@ -2,49 +2,45 @@ import { useState, useEffect } from 'react'
 import { Star } from '@phosphor-icons/react'
 import './Repositorio.css'
 
-export function Repositorio(){
+export function Repositorio() {
+  const token = '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'
 
-const token = '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'
+  const headers = {
+    'Authorization': `Token ${token}`
+  }
 
-const headers = {
-  'Authorization': `Token ${token}`
-}
+  const [repositorio, setRepositorio] = useState([])
 
-const [repositorio, setRepositorio] = useState([])
+  useEffect(() => {
+    async function carregaRepositorios() {
+      const res = await fetch(
+        "https://api.github.com/users/rehfeld13/repos", { headers }
+      )
 
-useEffect(()=>{
-  async function carregaRepositorios(){
-    const res = await fetch(
-      "https://api.github.com/users/rehfeld13/repos", {headers})
+      const repositorios = await res.json()
 
-    const repositorios = await res.json()
-
-    setRepositorio(repositorios)
+      setRepositorio(repositorios)
     }
 
-  carregaRepositorios()
+    carregaRepositorios()
+  }, [])
 
-}, [])
-
-  return(
+  return (
     <ul>
-      {repositorio.map((repositorio)=>{
-        return(
+      {repositorio.map((repositorio) => {
+        return (
           <li key={repositorio.id}>
-
             <span className='name'>{repositorio.name}</span>
             <span className='star'>
               {repositorio.stargazers_count}
               <Star />
             </span>
-
             <button>
-              <a target='_blank' href={repositorio.html_url}>Acesse aqui</a>
+              <a target='_blank' rel='noopener noreferrer' href={repositorio.html_url}>Acesse aqui</a>
             </button>
           </li>
         )
       })}
     </ul>
   )
-
 }
